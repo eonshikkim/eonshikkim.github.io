1f9680aa9b14a86871d16759daf7ab9e5c917404
Eonshik Kim <spykim2003@gmail.com>
2023-06-11 21:06:08 +0900

    update pictures and a work sample

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -1,9 +1,9 @@
 function changeImg() {
     const image = document.getElementById('myImg');
-    if (image.src.match("Assets/img/dummy1.png")) {
-        image.src = "Assets/img/dummy2.jpeg";
+    if (image.src.match("Assets/img/pic1.jpg")) {
+        image.src = "Assets/img/pic2.jpg";
     }
     else {
-        image.src = "Assets/img/dummy1.png";
+        image.src = "Assets/img/pic1.jpg";
     }
 }
